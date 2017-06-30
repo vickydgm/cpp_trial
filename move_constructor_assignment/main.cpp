c51f64dc67170281c7ff20088692e25d2a767218
Smruti  Ranjan Panda <vicky@Smrutis-MacBook-Pro.local>
2017-06-30 22:12:34 +0530

    Initial Commit

---
+++ move_constructor_assignment/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  move_constructor_assignment
+//
+//  Created by Smruti  Ranjan Panda on 30/06/17.
+//  Copyright © 2017 Smruti  Ranjan Panda. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
