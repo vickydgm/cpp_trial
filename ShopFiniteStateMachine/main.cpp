26b1e147563e752609679e014b8e7a4a08f4b9da
Smruti  Ranjan Panda <vicky@Smrutis-MacBook-Pro.local>
2017-07-15 12:25:47 +0530

    Initial Commit

---
+++ ShopFiniteStateMachine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ShopFiniteStateMachine
+//
+//  Created by Smruti  Ranjan Panda on 15/07/17.
+//  Copyright © 2017 Smruti  Ranjan Panda. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
